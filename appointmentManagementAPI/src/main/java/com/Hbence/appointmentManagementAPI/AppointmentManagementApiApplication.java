2f5a86c8a06920a258214531b1cd8453887141dd
Halmai Bence <93594678+Hbence10@users.noreply.github.com>
2025-08-19 15:29:08 +0200

    Spring boot projekt létrehozása
    A projekt a következő dependencyket tartalmazza:
     - Spring Web
     - Spring Data JPA
     - MySQL Driver
     - Spring Boot Actuator
     - Spring Boot DevTools
     - Java Mail Sender
     - Spring Security

---
+++ appointmentManagementAPI/src/main/java/com/Hbence/appointmentManagementAPI/AppointmentManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.Hbence.appointmentManagementAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppointmentManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppointmentManagementApiApplication.class, args);
+	}
+
+}
---
+++ appointmentManagementAPI/src/test/java/com/Hbence/appointmentManagementAPI/AppointmentManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Hbence.appointmentManagementAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointmentManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
