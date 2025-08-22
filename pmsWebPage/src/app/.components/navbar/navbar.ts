c1741c3e0f64c5f2ba816c71ae82a1766ecf1497
Halmai Bence <93594678+Hbence10@users.noreply.github.com>
2025-08-22 22:33:14 +0200

    kezdetleges navigáció hozzáadása a navbarhoz

--- pmsWebPage/src/app/.components/navbar/navbar.ts
+++ pmsWebPage/src/app/.components/navbar/navbar.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+// import { RouterLink } from "../../../../node_modules/@angular/router/router_module.d";
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss'
 })
--- pmsWebPage/src/app/app.routes.ts
+++ pmsWebPage/src/app/app.routes.ts
@@ -2,6 +2,28 @@ import { HomePage } from './.components/home-page/home-page';
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  {path: "homePage", component: HomePage, title: "Pécs Music Society - Főoldal"},
-  {path: "", pathMatch: "full", redirectTo: "homePage"}
+  { path: "homePage", component: HomePage, title: "Pécs Music Society - Főoldal" },
+  { path: "", pathMatch: "full", redirectTo: "homePage" },
+
+  //Lazy loadinggal betoltott componentek: Idopont foglalo/adminPage, Arlista, Felszereles, Velemenyek, Galleria
+  {
+    path: "priceList",
+    loadComponent: () => import("./.components/price-list/price-list").then(mod => mod.PriceList),
+    title: "Pécs Music Society - Árlista"
+  },
+  {
+    path: "equipments",
+    loadComponent: () => import("./.components/equipments/equipments").then(mod => mod.Equipments),
+    title: "Pécs Music Society - Felszerelés"
+  },
+  {
+    path: "reviews",
+    loadComponent: () => import("./.components/review/review").then(mod => mod.Review),
+    title: "Pécs Music Society - Vélemények"
+  },
+  {
+    path: "gallery",
+    loadComponent: () => import("./.components/gallery/gallery").then(mod => mod.Gallery),
+    title: "Pécs Music Society - Galléria"
+  }
 ];
