903442bc1e5f847050d43e34b12702cd565af0b8
Halmai Bence <93594678+Hbence10@users.noreply.github.com>
2025-08-22 20:28:07 +0200

    footer és navbar  component létrehozása

--- pmsWebPage/src/app/app.ts
+++ pmsWebPage/src/app/app.ts
@@ -1,9 +1,13 @@
+import { Navbar } from './.components/navbar/navbar';
+import { Footer } from './.components/footer/footer';
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+// import {FooterComp}
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Footer, Navbar],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
