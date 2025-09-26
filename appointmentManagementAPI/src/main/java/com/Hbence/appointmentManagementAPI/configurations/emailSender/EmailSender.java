ac87c17ef9dbe5081eb533ca849b6fbbb8ed8ffb
Halmai Bence <93594678+Hbence10@users.noreply.github.com>
2025-09-26 18:39:55 +0200

    sendVerificationCodeEmail function megirása

--- appointmentManagementAPI/src/main/java/com/Hbence/appointmentManagementAPI/configurations/emailSender/EmailSender.java
+++ appointmentManagementAPI/src/main/java/com/Hbence/appointmentManagementAPI/configurations/emailSender/EmailSender.java
@@ -1,5 +1,6 @@
 package com.Hbence.appointmentManagementAPI.configurations.emailSender;
 
+import com.Hbence.appointmentManagementAPI.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -10,17 +11,28 @@ import org.springframework.stereotype.Component;
 public class EmailSender {
 
     private final JavaMailSender mailSender;
-    private final String fromEmail = "";
+    private final String fromEmail = "sulisdolgok8@gmail.com";
+    private final UserRepository userRepository;
 
     public void sendEmailAboutRegistration() {
 
     }
 
-    public void sendVerificationCodeEmail(String toEmail, String verificationEmail) {
+    public void sendVerificationCodeEmail(String toEmail, String verificationCode) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(this.fromEmail);
+        message.setSubject("Hitelesitő kód");
+        message.setText("A hitelesitő kódja a jelszó változtatáshoz: " + verificationCode + ". \n A kód 5 percig aktív");
+        message.setTo(toEmail);
+
+        mailSender.send(message);
+    }
+
+    public void sendEmailAboutReservationCanceled() {
 
     }
 
-    public void sendEmailAboutReservationCanceled(){
+    public void sendEmailAboutNews(){
 
     }
 }
