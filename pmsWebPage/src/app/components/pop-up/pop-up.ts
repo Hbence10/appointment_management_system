517c5befe132c0ed743269e2b134cba1cd6d3bd3
Halmai Bence <93594678+Hbence10@users.noreply.github.com>
2025-12-01 20:06:47 +0100

    Revert "before update"
    This reverts commit fd79fe0560b16eeae4e5b12eb38c29d1f9bfefc0.

--- pmsWebPage/src/app/components/pop-up/pop-up.ts
+++ pmsWebPage/src/app/components/pop-up/pop-up.ts
@@ -456,13 +456,6 @@ export class PopUp implements OnInit {
   }
 
   updateRule(){
-    this.otherService.saveRuleChanges(this.otherService.rule!).subscribe({
-      next: response => console.log(response),
-      error: error => console.log(error),
-      complete: () => {
-        this.close()
-      }
-    })
     console.log("Szabályzat frissitése!")
   }
 }
